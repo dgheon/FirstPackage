03063cf67fa660fc6a4c91b0070e98455536059e
dgheon <grenier_heon.david@courrier.uqam.ca>
2019-05-03 11:25:59 -0400

    Committing my first package

---
+++ R/f.R
@@ -0,0 +1,19 @@
+#' My first package
+#'
+#' Small paragraph for description here
+#'
+#' @param x fisrt variable 
+#' @param y second variable
+#'
+#' @return 
+#' @export 
+#'
+#' @examples 
+f <- function(x, y) {
+  x + y
+}
+
+
+
+
+
